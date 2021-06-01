1823d86e4fd74c455ecb998cd08527b2faf488f1
Oad786 <36334253+Oad786@users.noreply.github.com>
2021-06-01 20:58:59 +0530

    Addition App

---
+++ app/src/main/java/com/example/addvalue/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.addvalue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText e1,e2;
+    TextView result;
+    Button add,clr;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        e1=findViewById(R.id.e1);
+        e2=findViewById(R.id.e2);
+        result=findViewById(R.id.result);
+        add=findViewById(R.id.add);
+        clr=findViewById(R.id.clr);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (e1.getText().toString().length()==0)
+                {
+                    e1.setText("0");
+                }
+                if (e2.getText().toString().length()==0)
+                {
+                    e2.setText("0");
+                }
+
+                int v1= Integer.parseInt(e1.getText().toString());
+                int v2= Integer.parseInt(e2.getText().toString());
+
+                int sum=v1+v2;
+                result.setText(String.valueOf(sum));
+            }
+        });
+        clr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                e1.setText("");
+                e2.setText("");
+                result.setText("");
+            }
+        });
+    }
+}
